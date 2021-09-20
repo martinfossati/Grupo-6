299b96c20e6e362341810c96231adaef78be2673
martinfossati <tinfossati@gmail.com>
2021-09-20 20:04:14 -0300

    Prueba config.js

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,14 +1,13 @@
-require("dotenv").config();
-
 module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "fansclub_db",
-    "host": "127.0.0.1",
+    "username": "fansclub",
+    "password": "fansclubdh2021",
+    "database": "fansclub_dh",
+    "host": "mysql-fansclub.alwaysdata.net",
     "dialect": "mysql",
     "port": 3306
   },
+  /*
   "test": {
     "username": "root",
     "password": null,
@@ -17,11 +16,11 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,
-    "dialect": process.env.DB_DIALECT,
-    "port": process.env.DB_PORT
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
   }
+  */
 }
