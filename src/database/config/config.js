738eb91865c96f82cb1b6e55e4208a137d7475ae
martinfossati <tinfossati@gmail.com>
2021-09-20 20:38:42 -0300

    Actualizamos el config.js

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,12 +1,14 @@
+require('dotenv').config();
+
 module.exports = {
   "development": {
-    "username": "fansclub",
-    "password": "fansclubdh2021",
-    "database": "fansclub_dh",
-    "host": "mysql-fansclub.alwaysdata.net",
-    "dialect": "mysql",
-    "port": 3306
-  },
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
+    "dialect": process.env.DB_DIALECT,
+    "port": process.env.DB_PORT
+  }
   /*
   "test": {
     "username": "root",
