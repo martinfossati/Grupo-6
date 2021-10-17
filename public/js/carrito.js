e1013e2af055e960737c61956a7d6be2739d8296
martinfossati <tinfossati@gmail.com>
2021-10-17 12:16:03 -0300

    Actualizamos el carrito

--- public/js/carrito.js
+++ public/js/carrito.js
@@ -12,35 +12,47 @@ window.addEventListener('load', () => {
             `<div class="items-carrito">
                 <img src="${item.img}" alt="" class="imagen-carrito">
                 <h5 class="nombre-carrito articulo">${item.nombre}</h5>
-                <h5 class="precio-carrito articulo">${item.precio}</h5>
-                <h5 class="total-carrito articulo">${item.precio}</h5>
+                <h5 class="precio-carrito articulo">$${item.precio}</h5>
+                <h5 class="total-carrito articulo">$${item.precio}</h5>
                 <i class="fas fa-trash borrar-item"></i>
             </div>`
         }
     }
 
-    // function resumenCarrito () {
-    //     let preciosItems = document.getElementsByClassName("precio-carrito");
+    let botonEliminar = document.getElementsByClassName('borrar-item');
+    
+    for (let i = 0; i < botonEliminar.length; i++){
+        botonEliminar[i].addEventListener('click', eliminarItem);
 
-    //     for (precio of preciosItems) {
-    //         let p = precio.innerText.replace('$', '');
-    //         totalItems += parseInt(p);
-    //         console.log(p);
-    //     }
+        function eliminarItem(){
 
-    //     let suma = 0;
-        
-    //     let totalItems = document.getElementById('total-items');
-    //     totalItems.innerText = ("$ " + suma);
+            let carritoActualizado = itemsArray.filter(item => item != itemsArray[i]);
+    
+            localStorage.setItem('productosCarrito', JSON.stringify(carritoActualizado));
 
-    //     let envioItems = document.getElementById('envio-items');
+            window.location.reload()
+        }
+    }
 
-    //     let sumatoria = totalItems + envioItems;
 
-    //     let sumatoriaTotal = document.getElementById('total-con-envio');
-    //     sumatoriaTotal.innerText = ("$ " + sumatoria);
-    // }
+    function resumenTotal() {
 
-    // resumenCarrito();
+        let sumatoria = 0;
+
+        for (let i = 0; i < itemsArray.length; i++) {
+            let precios = itemsArray[i].precio;
+            sumatoria += parseInt(precios);
+        }
+        let envioFijo = 1000;
+
+        let total = document.getElementById('total-items');
+        total.innerText = ('$' + sumatoria);
+
+        let sumaTotal = sumatoria + envioFijo;
+        let precioTotal = document.getElementById('total-con-envio');
+        precioTotal.innerText = ('$' + sumaTotal);
+    }
+
+    resumenTotal();
     
 })
